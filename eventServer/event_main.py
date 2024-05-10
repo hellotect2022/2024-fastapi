b45f621c2e6bd0830132a264ce5e7990952b84ed
dhhan <hellotect2022@gmail.com>
2024-05-10 18:12:49 +0900

    websocket html 생성

--- eventServer/event_main.py
+++ eventServer/event_main.py
@@ -7,13 +7,22 @@ import redis
 app = FastAPI()
 
 
-@app.websocket("/ws")
+@app.websocket("/test")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
-    i : int  = 1
-    # while True:
-    #     data = await websocket.receive_text()
-    #     print(f"웹소켓 : {data}")
+    redisClient = redis.StrictRedis(host="10.10.27.119", port=6379, db=0)
+    # 현재 사용 가능한 채널 조회
+    channels = redisClient.pubsub_channels()
+    # 조회된 채널 출력
+    print("Available channels:")
+    for channel in channels:
+        print(channel)
+
+    i: int = 1
+    while True:
+        data = await websocket.receive_text()
+        print(f"웹소켓 : {data}")
+        await websocket.send_text(f"Message text was: {data}")
 
 def test():
     redisClient = redis.StrictRedis(host="10.10.27.119", port=6379, db=0)
